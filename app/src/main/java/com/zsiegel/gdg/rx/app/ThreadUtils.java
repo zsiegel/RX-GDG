653791e4247d53cec9a79440d063a2ffcb55c94c
Zachary Siegel <zsiegel87@gmail.com>
2014-05-20 21:03:35 -0500

    format thread utils

--- app/src/main/java/com/zsiegel/gdg/rx/app/ThreadUtils.java
+++ app/src/main/java/com/zsiegel/gdg/rx/app/ThreadUtils.java
@@ -8,6 +8,8 @@ import android.os.Looper;
 public class ThreadUtils {
 
     public static void errorOnUIThread() {
-        if (Looper.myLooper() == Looper.getMainLooper()) throw new RuntimeException("This method should not be called on the main thread");
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            throw new RuntimeException("This method should not be called on the main thread");
+        }
     }
 }
